4371aea3ba161c752fb9eb218e414b83e4413b41
LeeSH0710 <dltmdgus0710@naver.com>
2023-06-30 16:52:28 +0900

    0630

--- project003/js/javascript.js
+++ project003/js/javascript.js
@@ -10,31 +10,31 @@ $(document).ready(function(){
     })
 
     $('header nav ul li:nth-child(1)').click(function(){
-        $('h1').css({'left':'-5%'}).css({'top':'50%'}).css({'transform':'rotate(55deg)'})
-        $('nav').css({'transform':'rotate(-35deg)'}).css({'right':'10%'})
+        $('h1').css({'left':'-5%','top':'50%','transform':'rotate(55deg)'})
+        $('nav').css({'transform':'rotate(-35deg)','right':'10%','top':'5%'})
         $('nav ul li').css({'transform':'rotate(35deg)'})
-        $('.box .boxx').css({'width':'60%'}).css({'left':'20%'}).css({'top':'-40%'}).css({'transform':'rotate(-35deg)'}).css({'height':'185%'})
+        $('.box .boxx').css({'width':'60%','left':'20%','top':'-40%','transform':'rotate(-35deg)','height':'185%'})
     })
 
     $('header nav ul li:nth-child(2)').click(function(){
 
         $('nav').css({'transform':'rotate(0deg)'})
-        $('nav').css({'right':'5%'})
+        $('nav').css({'right':'5%','top':'5%'})
         $('nav ul li').css({'transform':'rotate(0deg)'})
-        $('h1').css({'transform':'rotate(90deg)'}).css({'left':'-12%'}).css({'top':'35%'})
-        $('.box .boxx').css({'transform':'rotate(0)'}).css({'width':'60%'}).css({'left':'20%'}).css({'top':'-40%'}).css({'height':'185%'})
+        $('h1').css({'transform':'rotate(90deg)','left':'-12%','top':'35%'})
+        $('.box .boxx').css({'transform':'rotate(0)','width':'60%','left':'20%','top':'-40%','height':'185%'})
     })
 
     $('nav ul li:nth-child(3)').click(function(){
-        $('h1').css({'left':'-5%'}).css({'top':'20%'}).css({'transform':'rotate(-55deg)'})
-        $('nav').css({'transform':'rotate(35deg)'}).css({'right':'5%'}).css({'top':'50%'})
+        $('h1').css({'left':'-5%','top':'20%','transform':'rotate(-55deg)'})
+        $('nav').css({'transform':'rotate(35deg)','right':'5%','top':'50%'})
         $('nav ul li').css({'transform':'rotate(-35deg)'})
-        $('.box .boxx').css({'width':'60%'}).css({'left':'20%'}).css({'top':'-40%'}).css({'transform':'rotate(35deg)'}).css({'height':'185%'})
+        $('.box .boxx').css({'width':'60%','left':'20%','top':'-40%','transform':'rotate(35deg)','height':'185%'})
     })
 
     $('header nav ul li:nth-child(4)').click(function(){
         $('h1').css({'left':'35%'}).css({'top':'-8%'}).css({'transform':'rotate(0deg)'})
-        $('nav').css({'transform':'rotate(90deg)'}).css({'right':'20%'}).css({'top':'67%'})
+        $('nav').css({'transform':'rotate(90deg)','right':'20%','top':'67%'})
         $('nav ul li').css({'transform':'rotate(-90deg)'})
         $('.box .boxx').css({'width':'100%'}).css({'height':'70vh'}).css({'top':'15%'})
         $('.box .boxx').css({'width':'60%'}).css({'left':'20%'}).css({'transform':'rotate(0deg)'})
